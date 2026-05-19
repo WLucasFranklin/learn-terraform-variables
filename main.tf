5b394ca48fa1b3a1042fdba600dcfac626fa7190
wlucasfranklin <wlucasfranklin@gmail.com>
2026-05-19 13:35:33 -0400

    Change environment tag from dev to development

--- main.tf
+++ main.tf
@@ -11,12 +11,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.67"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.6"
+      version = "~> 3.23"
     }
   }
 }
